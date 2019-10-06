ec651892f89ae402ebaa7a076a51d49f7a8de2fc
takara1356 <goblin7283@gmail.com>
2019-10-06 21:46:06 +0900

    テンプレートの作成

---
+++ pages/template.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container
+    fill-height
+    fluid
+    grid-list-xl>
+    <v-layout
+      justify-center
+      wrap
+    >
+      <v-flex
+        xs12
+        md8
+      >
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import materialCard from '~/components/material/AppCard'
+
+  export default {
+    layout: 'dashboard',
+    components: {
+      materialCard
+    }
+  }
+</script>
